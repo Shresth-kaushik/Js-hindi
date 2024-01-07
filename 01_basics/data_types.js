247c6ebcc7f4a9557780647fe2267a453d5cbb34
Shresth Kaushik <107255313+Shresth-kaushik@users.noreply.github.com>
2024-01-07 12:58:31 +0000

    7_jan

---
+++ 01_basics/data_types.js
@@ -0,0 +1,13 @@
+let state; // undefined value hai
+
+// numbers (integer)
+// bigint => used when the number is not in the range of the integer 
+// boolean => true/false 
+// null => standalone value
+// undefined => 
+
+// Object -> user defined data type 
+
+console.log(typeof null); // object 
+console.log(typeof undefined); //undefined 
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 1244
+let namePerson = "Kaushik"
+let accountEmail = "ankit@gmail.com"
+var password = "14244"
+accontCity = "jaipur"
+
+// const variable can not be changed 
+// accountId = 1341  => Not valid syntax 
+
+console.log(accountId);
+accountEmail = "amit@gmail.com"
+password = "1451"
+/*
+Prefered not to use the var due to block scope and function scope 
+*/
+console.table([accountEmail ,accountId ,password ,namePerson]);
+
+ 
