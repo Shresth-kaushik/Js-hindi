0549f0ff042cfd5f25ace132457939da3c9700a6
Shresth Kaushik <107255313+Shresth-kaushik@users.noreply.github.com>
2024-01-18 05:40:50 +0000

    18_jan

--- 05_loops/map.js
+++ 05_loops/map.js
@@ -3,7 +3,7 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // const newNums = myNumers.map( (num) => { return num + 10})
 
 
-// chaining of the methods 
+// chaining of the methods can also be applied 
 const newNums = myNumers
                 .map((num) => num * 10 )
                 .map( (num) => num + 1)
